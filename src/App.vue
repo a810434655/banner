9e21fd8da7d2b538066e036a46b2570ef9fdbb93
a810434655 <woshiwuxingxing@qq.com>
2018-12-28 17:46:23 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+  <banner :carouselList="list"/>
+  </div>
+</template>
+
+<script>
+import banner from './components/banner'
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      list:[
+        {id:"1",img:"../../static/img/james_harden.jpg"},
+        {id:"2",img:"../../static/img/james_harden-002.jpg"},
+        {id:"3",img:"../../static/img/james_harden-010.jpg"},
+      ],
+
+    }
+  },
+  components: {
+    banner
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/banner.vue
@@ -0,0 +1,46 @@
+<template>
+ <div id="banenr">
+   <a><img :src="carouselList[0].img" style="display: block" alt=""></a>
+ </div>
+  <ul>
+    <li v-for="(item,key) in carouselList" :key="item.id"></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'banner',
+  data () {
+    return {
+      pos:0,
+      startmove:"",
+    }
+  },
+  mounted(){
+  },
+  //轮播组件接收组件的宽度、高度、定时器间隔时间、轮播图List
+  props:{
+    width:{
+      type:Number,
+      default:900
+    },
+    height:{
+      type:Number,
+      default:400
+    },
+    interval:{
+      type:Number,
+      default:2000
+    },
+    carouselList:{
+      type:Array,
+      default:function () {
+        return []
+      }
+    }
+  }
+}
+</script>
+<style>
+
+</style>
